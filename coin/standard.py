241729071b5ceb67e53df87f2e7438b6617850ad
Wei En <wei2912.supp0rt@gmail.com>
2014-04-14 20:20:06 +0800

    Overhaul of simulations. Includes graphs now.

--- coin/standard.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def run_trials(args):
-	hits = 0
-	for _ in xrange(args.trials):
-		x = random.uniform(0.0, args.gap)
-		hits += int(args.gap - x < args.radius or x < args.radius)
-	return hits
--- needle.py
+++ needle.py
@@ -1,46 +1,26 @@
-import argparse, multiprocessing, math
-from needle import mp, standard
+import random, math
 
-max_trials = 1e6 # max trials a job can handle
+class NeedleSim:
+	def __init__(self, length, gap, angle, trials):
+		self.length = length
+		self.gap = gap
+		self.angle = angle
+		self.trials = trials
 
-def main(args):
-	print("Length: %f" % args.length)
-	print("Gap length: %f" % args.gap)
-	if args.angle: print("Angle: %f" % args.angle)
-	print("")
-	print("Running %d trials..." % args.trials)
-	print("===")
+	def run_trials(self):
+		# if the angle is specified, precompute it
+		if self.angle:
+			oppval = self.length/2 * math.sin(self.angle)
 
-	if not args.process:
-		args.process = multiprocessing.cpu_count()
+		hits = 0
+		for _ in xrange(self.trials):
+			x = random.uniform(0.0, self.gap)
 
-	if args.process == 1 or args.trials < max_trials*args.process:
-		hits = standard.run_trials(args)
-	else:
-		hits = mp.run_trials(args, args.process, max_trials)
-	nonhits = args.trials - hits
+			if self.angle:
+				opposite = oppval
+			else:
+				angle = random.uniform(0.0, math.pi) # in radians
+				opposite = self.length/2 * math.sin(angle)
 
-	print("Number of hits: %d" % hits)
-	print("Number of non-hits: %d" % nonhits)
-	print("")
-
-	exp_prob = float(hits)/args.trials
-
-	if args.angle:
-		print("Experimental P(C) for angle %f = %f" % (args.angle, exp_prob))
-	else:
-		pred_prob = 2*args.length/args.gap/math.pi
-		print("Predicted P(C) = %f" % pred_prob)
-		print("Experimental P(C) = %f" % exp_prob)
-		error = math.fabs(100-exp_prob/pred_prob*100)
-		print("Error = %f" % error)
-
-parser = argparse.ArgumentParser(description="Buffon's Needle Simulation")
-parser.add_argument('-l', '--length', type=float, required=True, help='length of needle')
-parser.add_argument('-g', '--gap', type=float, required=True, help='length of gap between two lines')
-parser.add_argument('-a', '--angle', type=float, help='angle of needle. if not specified, program generates a random number as the angle.')
-parser.add_argument('-t', '--trials', type=int, default=1e7, help='number of trials to run')
-parser.add_argument('-p', '--process', type=int, help='number of processes to run')
-
-args = parser.parse_args()
-main(args)
+			hits += int(self.gap - x < opposite or x < opposite)
+		return hits
