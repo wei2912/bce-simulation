f13f14b134d76acac9cad8a93b47315fb0df1ba9
Wei En <wei2912.supp0rt@gmail.com>
2014-05-16 17:36:01 +0800

    Raise exception if step value is invalid.

--- utils/stepvals.py
+++ utils/stepvals.py
@@ -1,6 +1,9 @@
 import math
 
 def get_range(val, step):
+	if args.step >= val:
+		raise Exception("Step value is too large! Must be smaller than value.")
+
 	stepvals = [i*step for i in xrange(int(math.ceil(val/step)))][1:]
 	if not stepvals[-1] == val: # if last element isn't the actual value
 		stepvals += [val] # add it in
