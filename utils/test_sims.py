b82b62112a16a363c46138b46a746205bcce6305
Wei En <wei2912.supp0rt@gmail.com>
2014-05-25 23:06:45 +0800

    Clean up TestCoinSim

--- utils/test_sims.py
+++ utils/test_sims.py
@@ -8,22 +8,51 @@ TRIALS = 1000 # number of trials to run per test case
 MAX_DEGREE = 1.5 # max degree of freedom; refer to https://en.wikipedia.org/wiki/Pearson's_chi-squared_test
 
 class TestCoinSim(unittest.TestCase):
-	def runTest(self):
-		# bad input
-		self.assertRaises(Exception, sims.CoinSim, 0, 1, 1) # bad radius
-		self.assertRaises(Exception, sims.CoinSim, -1, 1, 1) # bad radius
-		self.assertRaises(Exception, sims.CoinSim, 1, 0, 1) # bad gap
-		self.assertRaises(Exception, sims.CoinSim, 1, -1, 1) # bad gap
-		self.assertRaises(Exception, sims.CoinSim, 1, 1, 0) # bad length
-		self.assertRaises(Exception, sims.CoinSim, 1, 1, -1) # bad length
+	def _predict_hits(self, radius, gap, trials):
+		pred_hits = self._predict_prob(radius, gap)*trials
+		return pred_hits
 
-		# if radius*2 >= gap length, it should always hit
-		hits = sims.CoinSim(1, 2, TRIALS).run_trials()
-		self.assertEquals(hits, TRIALS, "while radius*2 == gap, coin does not always hit")
+	def _predict_prob(self, radius, gap):
+		pred_prob = float(radius*2)/gap
+		if pred_prob > 1.0:
+			return 1.0
+		return pred_prob	
 
-		hits = sims.CoinSim(1, 1.5, TRIALS).run_trials()
-		self.assertEquals(hits, TRIALS, "while radius*2 > gap, coin does not always hit")
+	# if there's bad input, it should raise an exception
+	def bad_input_test(self):
+		self.assertRaises(Exception, sims.CoinSim, 0, 1, 1, "while radius == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 0, 1, "while gap == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 1, 0, "while length == 0, no exception was raised")
 
+		self.assertRaises(Exception, sims.CoinSim, -1, 1, 1, "while radius > 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, -1, 1, "while gap > 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 1, -1, "while length > 0, no exception was raised")
+
+	# if radius*2 >= gap length, it should always hit
+	def always_hit(self):
+		for i in range(1000):
+			radius = 0
+			while radius == 0:
+				radius = random.random()
+
+			hits = sims.CoinSim(radius, radius*2, TRIALS).run_trials()
+			self.assertEquals(hits, TRIALS, "while radius*2 == gap, coin does not always hit")
+
+		for i in range(1000):
+			radius = 0
+			while radius == 0:
+				radius = random.random()
+
+			gap = radius*2
+			while gap >= radius*2 or gap < 0:
+				gap = radius*2 - random.random()
+
+			hits = sims.CoinSim(radius, gap, TRIALS).run_trials()
+			self.assertEquals(hits, TRIALS, "while radius*2 > gap, coin does not always hit")
+
+	# if radius*2 < gap length
+	# average degree of freedom should be < MAX_DEGREE
+	def is_uniform_random(self):
 		degrees = []
 		for i in range(1000):
 			radius = random.random()
@@ -47,15 +76,10 @@ class TestCoinSim(unittest.TestCase):
 		avg_degree = sum(degrees)/len(degrees)
 		self.assertTrue(avg_degree < MAX_DEGREE, "while diameter < gap, average degree of freedom is %f > %f" % (avg_degree, MAX_DEGREE))
 
-	def _predict_hits(self, radius, gap, trials):
-		pred_hits = self._predict_prob(radius, gap)*trials
-		return pred_hits
+	def runTest(self):
+		self.bad_input_test()
+		self.always_hit()
+		self.is_uniform_random()
 
-	def _predict_prob(self, radius, gap):
-		pred_prob = float(radius*2)/gap
-		if pred_prob > 1.0:
-			return 1.0
-		return pred_prob
-		
 if __name__ == '__main__':
 	unittest.main()
