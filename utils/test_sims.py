1f7817a56e429e0d087bd37e7c89d7803e94d7e0
Wei En <wei2912.supp0rt@gmail.com>
2014-05-25 23:21:06 +0800

    Add TestCoinGridSim

--- utils/test_sims.py
+++ utils/test_sims.py
@@ -13,7 +13,8 @@ class TestCoinSim(unittest.TestCase):
 		return pred_hits
 
 	def _predict_prob(self, radius, gap):
-		pred_prob = float(radius*2)/gap
+		region = radius*2 # area of region which coin would hit
+		pred_prob = region/gap
 		if pred_prob > 1.0:
 			return 1.0
 		return pred_prob	
@@ -28,7 +29,7 @@ class TestCoinSim(unittest.TestCase):
 		self.assertRaises(Exception, sims.CoinSim, 1, -1, 1, "while gap > 0, no exception was raised")
 		self.assertRaises(Exception, sims.CoinSim, 1, 1, -1, "while length > 0, no exception was raised")
 
-	# if radius*2 >= gap length, it should always hit
+	# if diameter >= gap length, it should always hit
 	def always_hit(self):
 		for i in range(1000):
 			radius = 0
@@ -36,7 +37,7 @@ class TestCoinSim(unittest.TestCase):
 				radius = random.random()
 
 			hits = sims.CoinSim(radius, radius*2, TRIALS).run_trials()
-			self.assertEquals(hits, TRIALS, "while radius*2 == gap, coin does not always hit")
+			self.assertEquals(hits, TRIALS, "while diameter == gap, coin does not always hit")
 
 		for i in range(1000):
 			radius = 0
@@ -48,9 +49,9 @@ class TestCoinSim(unittest.TestCase):
 				gap = radius*2 - random.random()
 
 			hits = sims.CoinSim(radius, gap, TRIALS).run_trials()
-			self.assertEquals(hits, TRIALS, "while radius*2 > gap, coin does not always hit")
+			self.assertEquals(hits, TRIALS, "while diameter > gap, coin does not always hit")
 
-	# if radius*2 < gap length
+	# if diameter < gap length
 	# average degree of freedom should be < MAX_DEGREE
 	def is_uniform_random(self):
 		degrees = []
@@ -62,19 +63,105 @@ class TestCoinSim(unittest.TestCase):
 				radius = random.random()
 				gap = random.random()
 
-			# if radius*2 < gap length
-			# average degree of freedom should be < MAX_DEGREE
-			else:
-				hits = sims.CoinSim(radius, gap, TRIALS).run_trials()
-				pred_hits = self._predict_hits(radius, gap, TRIALS)
+			hits = sims.CoinSim(radius, gap, TRIALS).run_trials()
+			pred_hits = self._predict_hits(radius, gap, TRIALS)
 
-				# refer to https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
-				# below equation was simplified after substituting original
-				chi_squared = 2*((hits - pred_hits)**2)/pred_hits
-				degrees.append(chi_squared)
+			# refer to https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
+			# below equation was simplified after substituting original
+			chi_squared = 2*((hits - pred_hits)**2)/pred_hits
+			degrees.append(chi_squared)
 
 		avg_degree = sum(degrees)/len(degrees)
 		self.assertTrue(avg_degree < MAX_DEGREE, "while diameter < gap, average degree of freedom is %f > %f" % (avg_degree, MAX_DEGREE))
+
+	def runTest(self):
+		self.bad_input_test()
+		self.always_hit()
+		self.is_uniform_random()
+
+class TestCoinGridSim(unittest.TestCase):
+	def _predict_hits(self, radius, gap_x, gap_y, trials):
+		pred_hits = self._predict_prob(radius, gap_x, gap_y)*trials
+		return pred_hits
+
+	def _predict_prob(self, radius, gap_x, gap_y):
+		region = gap_x*gap_y - (gap_x-radius*2)*(gap_y-radius*2) # area of region which coin would hit
+		pred_prob = region/(gap_x * gap_y)
+		if pred_prob > 1.0:
+			return 1.0
+		return pred_prob	
+
+	# if there's bad input, it should raise an exception
+	def bad_input_test(self):
+		self.assertRaises(Exception, sims.CoinGridSim, 0, 1, 1, 1, "while radius == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinGridSim, 1, 0, 1, 1, "while gap_x == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinGridSim, 1, 1, 0, 1, "while gap_y == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinGridSim, 1, 1, 1, 0, "while length == 0, no exception was raised")
+
+		self.assertRaises(Exception, sims.CoinGridSim, -1, 1, 1, 1, "while radius < 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinGridSim, 1, -1, 1, 1, "while gap_x < 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinGridSim, 1, 1, -1, 1, "while gap_y < 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinGridSim, 1, 1, 1, -1, "while length < 0, no exception was raised")
+
+	# if diameter >= gap_x or diameter >= gap_y, it should always hit
+	def always_hit(self):
+		for i in range(1000):
+			radius = 0
+			while radius == 0:
+				radius = random.random()
+
+			more_gap = radius*2
+			while more_gap == radius*2:
+				more_gap = radius*2 + random.random()
+
+			hits = sims.CoinGridSim(radius, radius*2, more_gap, TRIALS).run_trials()
+			self.assertEquals(hits, TRIALS, "while diameter == gap_x, coin does not always hit")
+
+			hits = sims.CoinGridSim(radius, more_gap, radius*2, TRIALS).run_trials()
+			self.assertEquals(hits, TRIALS, "while diameter == gap_y, coin does not always hit")
+
+		for i in range(1000):
+			radius = 0
+			while radius == 0:
+				radius = random.random()
+
+			less_gap = radius*2
+			while less_gap >= radius*2 or less_gap < 0:
+				less_gap = radius*2 - random.random()
+			more_gap = radius*2
+			while more_gap == radius*2:
+				more_gap = radius*2 + random.random()
+
+			hits = sims.CoinGridSim(radius, less_gap, more_gap, TRIALS).run_trials()
+			self.assertEquals(hits, TRIALS, "while diameter> gap_x, coin does not always hit")
+
+			hits = sims.CoinGridSim(radius, more_gap, less_gap, TRIALS).run_trials()
+			self.assertEquals(hits, TRIALS, "while diameter > gap_y, coin does not always hit")
+
+	# if diameter < gap_x and diameter < gap_y
+	# average degree of freedom should be < MAX_DEGREE
+	def is_uniform_random(self):
+		degrees = []
+		for i in range(1000):
+			radius = random.random()
+			gap_x = random.random()
+			gap_y = random.random()
+
+			while radius*2 >= gap_x or radius*2 >= gap_y:
+				radius = random.random()
+				gap_x = random.random()
+				gap_y = random.random()
+
+			hits = sims.CoinGridSim(radius, gap_x, gap_y, TRIALS).run_trials()
+			pred_hits = self._predict_hits(radius, gap_x, gap_y, TRIALS)
+
+			# refer to https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
+			# below equation was simplified after substituting original
+			chi_squared = 2*((hits - pred_hits)**2)/pred_hits
+			degrees.append(chi_squared)
+
+		avg_degree = sum(degrees)/len(degrees)
+		self.assertTrue(avg_degree < MAX_DEGREE, "while diameter < gap_x and diameter < gap_y, average degree of freedom is %f > %f" % (avg_degree, MAX_DEGREE))
 
 	def runTest(self):
 		self.bad_input_test()
