2c153f3b94f7afc04afa2e5e7abce882e0f1981b
Wei En <wei2912.supp0rt@gmail.com>
2014-05-25 22:55:07 +0800

    Add exception handling.

--- utils/sims.py
+++ utils/sims.py
@@ -1,8 +1,21 @@
 import random
 import math
 
+class InvalidInput(Exception):
+	def __init__(self, input_var):
+		self.input_var = input_var
+	def __str__(self):
+		return "%s must not be <= 0." % self.input_var
+
 class CoinSim:
 	def __init__(self, radius, gap, trials):
+		if radius <= 0:
+			raise InvalidInput("radius")
+		if gap <= 0:
+			raise InvalidInput("gap")
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		self.radius = radius  # Radius of the coin
 		self.gap = gap        # Gap between the two lines that the coin might fall on
 		self.trials = trials  # Number of times to test the coin drop
@@ -20,6 +33,15 @@ class CoinSim:
 
 class CoinGridSim:
 	def __init__(self, radius, gap_x, gap_y, trials):
+		if radius <= 0:
+			raise InvalidInput("radius")
+		if gap_x <= 0:
+			raise InvalidInput("gap_x")
+		if gap_y <= 0:
+			raise InvalidInput("gap_y")
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		self.radius = radius  # Radius of the coin
 		self.gap_x = gap_x    # Horizontal gap between the two lines that the coin might fall on
 		self.gap_y = gap_y    # Vertical gap between the two lines that the coin might fall on
@@ -39,6 +61,13 @@ class CoinGridSim:
 
 class NeedleSim:
 	def __init__(self, length, gap, trials):
+		if length <= 0:
+			raise InvalidInput("length")
+		if gap <= 0:
+			raise InvalidInput("gap")
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		self.length = length  # Length of the needle
 		self.gap = gap        # Gap between the two lines where the needle falls
 		self.trials = trials  # Number of times to test the needle drop
@@ -54,10 +83,20 @@ class NeedleSim:
 
 			if self.gap - x < opp or x < opp:
 				hits += 1
+
 		return hits
 
 class NeedleAngleSim:
 	def __init__(self, length, gap, angle, trials):
+		if length <= 0:
+			raise InvalidInput("length")
+		if gap <= 0:
+			raise InvalidInput("gap")
+		if angle <= 0:
+			raise InvalidInput("angle")
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		self.length = length  # Length of the needle
 		self.gap = gap        # Gap between the two lines where the needle falls
 		self.angle = angle    # Angle of needle
