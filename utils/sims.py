f35476950909e2142136d02e91ca116b90b6b21f
Wei En <wei2912.supp0rt@gmail.com>
2014-06-01 18:59:02 +0800

    Update sims to use the correct way of checking if theoretical matches experimental & shift trials from init function to run_trials().

--- utils/sims.py
+++ utils/sims.py
@@ -8,25 +8,25 @@ class InvalidInput(Exception):
 		return "%s must not be <= 0." % self.input_var
 
 class CoinSim:
-	def __init__(self, radius, gap_x, gap_y, trials):
+	def __init__(self, radius, gap_x, gap_y):
 		if radius <= 0:
 			raise InvalidInput("radius")
 		if gap_x <= 0:
 			raise InvalidInput("gap_x")
 		if gap_y <= 0:
 			raise InvalidInput("gap_y")
-		if trials <= 0:
-			raise InvalidInput("trials")
 
 		self.radius = radius  # Radius of the coin
 		self.gap_x = gap_x    # Horizontal gap between the two lines that the coin might fall on
 		self.gap_y = gap_y    # Vertical gap between the two lines that the coin might fall on
-		self.trials = trials  # Number of times to test the coin drop
 
-	def run_trials(self):
+	def run_trials(self, trials):
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		hits = 0
 
-		for _ in xrange(self.trials):
+		for _ in xrange(trials):
 			x = random.uniform(0.0, self.gap_x) # location of coin along x-axis after drop
 			y = random.uniform(0.0, self.gap_y) # location of coin along y-axis after drop
 
@@ -36,22 +36,22 @@ class CoinSim:
 		return hits
 
 class NeedleSim:
-	def __init__(self, length, gap, trials):
+	def __init__(self, length, gap):
 		if length <= 0:
 			raise InvalidInput("length")
 		if gap <= 0:
 			raise InvalidInput("gap")
-		if trials <= 0:
-			raise InvalidInput("trials")
 
 		self.length = length  # Length of the needle
 		self.gap = gap        # Gap between the two lines where the needle falls
-		self.trials = trials  # Number of times to test the needle drop
 
-	def run_trials(self):
+	def run_trials(self, trials):
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		hits = 0
 
-		for _ in xrange(self.trials):
+		for _ in xrange(trials):
 			x = random.uniform(0.0, self.gap) # location of needle after drop
 
 			angle = random.uniform(0.0, math.pi) # angle of needle in radians
@@ -63,28 +63,28 @@ class NeedleSim:
 		return hits
 
 class NeedleAngleSim:
-	def __init__(self, length, gap, angle, trials):
+	def __init__(self, length, gap, angle):
 		if length <= 0:
 			raise InvalidInput("length")
 		if gap <= 0:
 			raise InvalidInput("gap")
 		if angle <= 0:
 			raise InvalidInput("angle")
-		if trials <= 0:
-			raise InvalidInput("trials")
 
 		self.length = length  # Length of the needle
 		self.gap = gap        # Gap between the two lines where the needle falls
 		self.angle = angle    # Angle of needle
-		self.trials = trials  # Number of times to test the needle drop
 
 	def run_trials(self):
+		if trials <= 0:
+			raise InvalidInput("trials")
+
 		# since the angle is specified, precompute the opposite
 		opp = self.length/2 * math.sin(self.angle)
 
 		hits = 0
 
-		for _ in xrange(self.trials):
+		for _ in xrange(trials):
 			x = random.uniform(0.0, self.gap) # location of needle after drop
 
 			if self.gap - x < opp or x < opp:
--- utils/test_sims.py
+++ utils/test_sims.py
@@ -4,97 +4,92 @@ import math
 
 import sims
 
-TRIALS = 1000 # number of trials to run per test case
-MAX_DEGREE = 1.0 # max degree of freedom; refer to https://en.wikipedia.org/wiki/Pearson's_chi-squared_test
+TRIALS = 10000 # number of trials to run per test case
+MAX_STAT = 3.841 # 0.05 level of significance, 1 degree of freedom
 
 class TestCoinSim(unittest.TestCase):
-	def _predict_hits(self, radius, gap_x, gap_y, trials):
-		pred_hits = self._predict_prob(radius, gap_x, gap_y)*trials
-		return pred_hits
-
 	def _predict_prob(self, radius, gap_x, gap_y):
-		region = gap_x*gap_y - (gap_x-radius*2)*(gap_y-radius*2) # area of region which coin would hit
+		diameter = radius*2
+		if diameter > gap_x or diameter > gap_y:
+			return 1.0 # will always touch
+
+		region = gap_x*gap_y - (gap_x-diameter)*(gap_y-diameter) # area of region which coin would hit
 		pred_prob = region/(gap_x * gap_y)
-		if pred_prob > 1.0:
-			return 1.0
 		return pred_prob
 
 	# if there's bad input, it should raise an exception
 	def bad_input_test(self):
-		self.assertRaises(Exception, sims.CoinSim, 0, 1, 1, 1, "while radius == 0, no exception was raised")
-		self.assertRaises(Exception, sims.CoinSim, 1, 0, 1, 1, "while gap_x == 0, no exception was raised")
-		self.assertRaises(Exception, sims.CoinSim, 1, 1, 0, 1, "while gap_y == 0, no exception was raised")
-		self.assertRaises(Exception, sims.CoinSim, 1, 1, 1, 0, "while length == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 1, 1, "while radius == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 0, 1, "while gap_x == 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 1, 0, "while gap_y == 0, no exception was raised")
 
-		self.assertRaises(Exception, sims.CoinSim, -1, 1, 1, 1, "while radius < 0, no exception was raised")
-		self.assertRaises(Exception, sims.CoinSim, 1, -1, 1, 1, "while gap_x < 0, no exception was raised")
-		self.assertRaises(Exception, sims.CoinSim, 1, 1, -1, 1, "while gap_y < 0, no exception was raised")
-		self.assertRaises(Exception, sims.CoinSim, 1, 1, 1, -1, "while length < 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, -1, 1, 1, "while radius < 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, -1, 1, "while gap_x < 0, no exception was raised")
+		self.assertRaises(Exception, sims.CoinSim, 1, 1, -1, "while gap_y < 0, no exception was raised")
+		
+		good_sim = sims.CoinSim(1, 1, 1)
+		self.assertRaises(Exception, good_sim.run_trials, 0, "while trials == 0, no exception was raised")
+		self.assertRaises(Exception, good_sim.run_trials, -1, "while trials < 0, no exception was raised")
 
 	# if diameter >= gap_x or diameter >= gap_y, it should always hit
 	def always_hit(self):
-		for i in range(1000):
+		for i in range(10):
 			radius = 0
 			while radius == 0:
-				radius = random.random()
+				radius += random.random()
+			diameter = radius*2
 
-			more_gap = radius*2
-			while more_gap == radius*2:
-				more_gap = radius*2 + random.random()
+			more_gap = diameter
+			while more_gap == diameter:
+				more_gap += random.random()
 
-			hits = sims.CoinSim(radius, radius*2, more_gap, TRIALS).run_trials()
+			hits = sims.CoinSim(radius, diameter, more_gap).run_trials(TRIALS)
 			self.assertEquals(hits, TRIALS, "while diameter == gap_x, coin does not always hit")
 
-			hits = sims.CoinSim(radius, more_gap, radius*2, TRIALS).run_trials()
+			hits = sims.CoinSim(radius, more_gap, diameter).run_trials(TRIALS)
 			self.assertEquals(hits, TRIALS, "while diameter == gap_y, coin does not always hit")
 
-		for i in range(1000):
+		for i in range(10):
 			radius = 0
 			while radius == 0:
 				radius = random.random()
+			diameter = radius*2
 
-			less_gap = radius*2
-			while less_gap >= radius*2 or less_gap < 0:
-				less_gap = radius*2 - random.random()
-			more_gap = radius*2
-			while more_gap == radius*2:
-				more_gap = radius*2 + random.random()
+			less_gap = diameter
+			while less_gap == diameter:
+				less_gap -= random.uniform(0.0, diameter)
+			more_gap = diameter
+			while more_gap == diameter:
+				more_gap = diameter + random.random()
 
-			hits = sims.CoinSim(radius, less_gap, more_gap, TRIALS).run_trials()
-			self.assertEquals(hits, TRIALS, "while diameter> gap_x, coin does not always hit")
+			hits = sims.CoinSim(radius, less_gap, more_gap).run_trials(TRIALS)
+			self.assertEquals(hits, TRIALS, "while diameter > gap_x, coin does not always hit")
 
-			hits = sims.CoinSim(radius, more_gap, less_gap, TRIALS).run_trials()
+			hits = sims.CoinSim(radius, more_gap, less_gap).run_trials(TRIALS)
 			self.assertEquals(hits, TRIALS, "while diameter > gap_y, coin does not always hit")
 
 	# if diameter < gap_x and diameter < gap_y
 	# average degree of freedom should be < MAX_DEGREE
-	def is_uniform_random(self):
-		degrees = []
-		for i in range(1000):
-			radius = random.random()
+	def is_match_theoretical(self):
+		for i in range(10):
 			gap_x = random.random()
 			gap_y = random.random()
+			diameter = random.uniform(0.0, min(gap_x, gap_y))
+			radius = diameter/2
 
-			while radius*2 >= gap_x or radius*2 >= gap_y:
-				radius = random.random()
-				gap_x = random.random()
-				gap_y = random.random()
+			hits = sims.CoinSim(radius, gap_x, gap_y).run_trials(TRIALS)
+			pred_hits = self._predict_prob(radius, gap_x, gap_y)*TRIALS
 
-			hits = sims.CoinSim(radius, gap_x, gap_y, TRIALS).run_trials()
-			pred_hits = self._predict_hits(radius, gap_x, gap_y, TRIALS)
-
-			# refer to https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
-			# below equation was simplified after substituting original
 			chi_squared = ((hits - pred_hits)**2)/pred_hits
-			degrees.append(chi_squared)
+			print("(%f, %f, %f) %d ~ %f, %f" % (radius, gap_x, gap_y, hits, pred_hits, chi_squared))
+			assert(chi_squared >= 0)
 
-		avg_degree = sum(degrees)/len(degrees)
-		self.assertTrue(avg_degree < MAX_DEGREE, "while diameter < gap_x and diameter < gap_y, average degree of freedom is %f > %f" % (avg_degree, MAX_DEGREE))
+			self.assertTrue(chi_squared < MAX_STAT, "while diameter < gap_x and diameter < gap_y, average chi^2 stat is %f >= %f" % (chi_squared, MAX_STAT))
 
 	def runTest(self):
 		self.bad_input_test()
 		self.always_hit()
-		self.is_uniform_random()
+		self.is_match_theoretical()
 
 if __name__ == '__main__':
 	unittest.main()
