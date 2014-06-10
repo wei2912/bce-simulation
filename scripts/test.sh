5d2c6e58b394696d3495c96678b6602f4f55cc88
Wei En <wei2912.supp0rt@gmail.com>
2014-06-10 21:37:23 +0800

    Add script to run tests and add pylint.

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "##############"
+echo "# unit tests #"
+echo "##############"
+
+python -m unittest utils.test_sims || exit 1
+
+echo "#################"
+echo "# pylint report #"
+echo "#################"
+
+pylint utils *.py
+
+exit 0
