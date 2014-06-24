9f40212a380330ae6ce4dd7624f77910d2898a3f
Wei En <wei2912.supp0rt@gmail.com>
2014-06-24 21:28:54 +0800

    derrrrrrrp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 gpg --passphrase $PASSWORD -o ~/.ssh/id_rsa -d scripts/deploy_rsa.gpg
-cp scripts/known_hosts ~/.ssh/known_hosts
+cat known_hosts >> ~/.ssh/known_hosts
 
-ls -al ~/.ssh
+cat ~/.ssh/known_hosts
 
 case "$TRAVIS_BRANCH" in
   master)
     git remote add heroku git@heroku.com:bce-simulation.git
-    git push heroku master 
+    git push heroku master
     ;;
 esac
 
