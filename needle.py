b4a38aa6aff569184b1ab128e3166b37cecff8e2
Wei En <wei2912.supp0rt@gmail.com>
2014-07-20 02:34:57 +0800

    Fix some bugs.

--- needle.py
+++ needle.py
@@ -14,39 +14,6 @@ from utils.sims import NeedleSim
 def _get_prob(hits, trials):
     return float(hits)/trials
 
-def plot_length(args):
-    """
-    Plots a 2D scatter plot which shows the
-    relationship between the length of the
-    needle and the probability which the needle
-    hits at least one of the two parallel lines.
-    """
-
-    vals = stepvals.get_range(args.length, 1000)
-    expected = []
-    for length in vals:
-        sim = NeedleSim(length, args.gap)
-        expected.append(sim.predict_prob())
-
-    graph.line_plot(vals, expected)
-
-    vals = stepvals.get_range(args.length, args.stepsize)
-    for length in vals:
-        sim = NeedleSim(length, args.gap)
-        prob = _get_prob(sim.run_trials(args.trials), args.trials)
-
-        if args.verbose:
-            print "length = %.5g, gap width = %.5g: %.5g" % (length, args.gap, prob)
-        graph.scatter_plot(length, prob)
-
-    graph.scale_plot(args.length, args.stepsize)
-    graph.prepare_plot(
-        "Length of needle",
-        "Probability of needle touching a line",
-        "Buffon's Needle Experiment" +
-            "\ngap width = %.5g" % args.gap
-    )
-
 def plot_gap(args):
     """
     Plots a 2D scatter plot which shows the
@@ -81,15 +48,48 @@ def plot_gap(args):
             "\nlength = %.5g" % args.length
     )
 
+def plot_length(args):
+    """
+    Plots a 2D scatter plot which shows the
+    relationship between the length of the
+    needle and the probability which the needle
+    hits at least one of the two parallel lines.
+    """
+
+    vals = stepvals.get_range(args.length, 1000)
+    expected = []
+    for length in vals:
+        sim = NeedleSim(length, args.gap)
+        expected.append(sim.predict_prob())
+
+    graph.line_plot(vals, expected)
+
+    vals = stepvals.get_range(args.length, args.stepsize)
+    for length in vals:
+        sim = NeedleSim(length, args.gap)
+        prob = _get_prob(sim.run_trials(args.trials), args.trials)
+
+        if args.verbose:
+            print "length = %.5g, gap width = %.5g: %.5g" % (length, args.gap, prob)
+        graph.scatter_plot(length, prob)
+
+    graph.scale_plot(args.length, args.stepsize)
+    graph.prepare_plot(
+        "Length of needle",
+        "Probability of needle touching a line",
+        "Buffon's Needle Experiment" +
+            "\ngap width = %.5g" % args.gap
+    )
+
 MODES = {
-    0: plot_length,
-    1: plot_gap
+    0: plot_gap,
+    1: plot_length
 }
 
 MODES_TXT = [
     'mode determines what type of graph to plot.',
-    'mode 0: 2D scatter plot, length of needle against probability of needle touching a line',
-    'mode 1: 2D scatter plot, gap width against probability of needle touching a line'
+    'mode 0: 2D scatter plot, gap width against probability of needle touching a line',
+    'mode 1: 2D scatter plot, length of needle against probability of needle touching a line'
 ]
 
 def _run_handler(args):
