cd387f70a8e038ee8e51455fc60e51e4104802ea
Wei En <wei2912.supp0rt@gmail.com>
2014-12-19 22:43:28 +0800

    Use native Andrew's monotone chain convex hull algorithm.

--- utils/coin_var.py
+++ utils/coin_var.py
@@ -8,9 +8,50 @@ to touching one of the lines of the grid.
 import random
 import math
 
-from scipy.spatial import ConvexHull 
-
 DEFAULT_TRIALS = 100000
+
+def __convex__hull(points):
+    """Computes the convex hull of a set of 2D points.
+ 
+    Input: an iterable sequence of (x, y) pairs representing the points.
+    Output: a list of vertices of the convex hull in counter-clockwise order,
+      starting from the vertex with the lexicographically smallest coordinates.
+    Implements Andrew's monotone chain algorithm. O(n log n) complexity.
+
+    Taken from https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain
+    """
+ 
+    # Sort the points lexicographically (tuples are compared lexicographically).
+    # Remove duplicates to detect the case we have just one unique point.
+    points = sorted(set(points))
+ 
+    # Boring case: no points or a single point, possibly repeated multiple times.
+    if len(points) <= 1:
+        return points
+ 
+    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+    # Returns a positive value, if OAB makes a counter-clockwise turn,
+    # negative for clockwise turn, and zero if the points are collinear.
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+ 
+    # Build lower hull 
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+ 
+    # Build upper hull
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+ 
+    # Concatenation of the lower and upper hulls gives the convex hull.
+    # Last point of each list is omitted because it is repeated at the beginning of the other list. 
+    return lower[:-1] + upper[:-1]
 
 def __transform_center(gap_width, x, y):
     """
@@ -88,12 +129,12 @@ def run_trials(diameter=1.0, gap_width=1.0, trials=DEFAULT_TRIALS):
         # if it is, the coin does not balance.
         # otherwise, the coin does.
         pivots.append([x, y])
-        hull = ConvexHull(pivots)
+        hull = __convex__hull(pivots)
 
         # if center is in the convex hull
         # whee we have a hit
         center_index = len(pivots) - 1
-        if not center_index in hull.vertices:
+        if not center_index in hull:
             hits += 1
 
     return hits
